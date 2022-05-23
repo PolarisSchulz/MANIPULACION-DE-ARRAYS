7576eae4c6cdbe6297a71cef49f3df5872c833d0
Abigail Peña <abigaildisenografico@gmail.com>
2022-05-23 01:07:14 -0500

    mapg en html

--- MANIPULACION DE ARRAYS/server/01-for-each.js
+++ MANIPULACION DE ARRAYS/server/01-for-each.js
@@ -1,10 +1,14 @@
 // para ver el resultado podemos hacerlo en la consola directa con runner (extension)
 
-const letters = ["a", "b", "c"];
+// Con un ciclo for
+// const letters = ["a", "b", "c"];
 
-for (let index = 0; index < letters.length; index++) {
-  const element = letters[index];
-  console.log("con for", element);
-}
+// for (let index = 0; index < letters.length; index++) {
+//   const element = letters[index];
+//   console.log("con for", element);
+// }
+
+// Con un forEach
+const letters = ["a", "b", "c"];
 
 letters.forEach((item) => console.log("con forEach", item));
