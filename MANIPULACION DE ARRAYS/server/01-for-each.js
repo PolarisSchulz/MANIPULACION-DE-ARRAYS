970ed4f64610e420b1e303f911b1e2a37babdbad
Abigail Peña <abigaildisenografico@gmail.com>
2022-05-25 20:45:48 -0500

    for each y map

--- MANIPULACION DE ARRAYS/server/01-for-each.js
+++ MANIPULACION DE ARRAYS/server/01-for-each.js
@@ -1,14 +1,20 @@
 // para ver el resultado podemos hacerlo en la consola directa con runner (extension)
 
+// - For Each  
+//     - Nos sirve para hacer un recorrido de los elementos de un array
+//     - Solo hay que enviar la función usando ⇒ (arrow function), y dentro de esto hacer el algoritmo o ejecución.
+//     - ⇒ Arrow Function: La funcion que se va a ejecutar por cada uno de los elementos. 
+//     - For Each hace lo mismo que un For, pero como metodo solo se ocupa en arrays  
+
 // Con un ciclo for
-// const letters = ["a", "b", "c"];
-
-// for (let index = 0; index < letters.length; index++) {
-//   const element = letters[index];
-//   console.log("con for", element);
-// }
-
-// Con un forEach
 const letters = ["a", "b", "c"];
 
-letters.forEach((item) => console.log("con forEach", item));
+for (let index = 0; index < letters.length; index++) {
+const element = letters[index];
+console.log("con for", element);
+}
+
+// Con un forEach
+const letters2 = ["a", "b", "c"];
+
+letters2.forEach((item) => console.log("con forEach", item));
--- MANIPULACION DE ARRAYS/server/02-map.js
+++ MANIPULACION DE ARRAYS/server/02-map.js
@@ -1,19 +1,32 @@
-// // Manera de mutar con un ciclo for
-// const letters = ["a", "b", "c"];
+// - Map   
+//     - Funciona para hacer transformaciones en arrays
+//     - .map() es INMUTABLE por lo tanto no modifica el array original, sino que crea uno nuevo con la “transformación” aplicada.   
+//     - Mantienes el mismo  número de elementos (length) que el array original.
+//     - Te devuelve en el nuevo array.
 
-// const newArray = [];
-// for (let index = 0; index < letters.length; index++) {
-//   const element = letters[index];
-//   newArray.push(element + '++');
-// }
+// Manera de mutar con un ciclo for
+const letters3 = ["a", "b", "c"];
 
-// console.log('original',letters);
-// console.log('arrayVacia',newArray);
+const newArray = [];
+for (let index = 0; index < letters.length; index++) {
+  const element = letters3[index];
+  newArray.push(element + '++');
+}
+
+console.log('original',letters);
+console.log('arrayVacia',newArray);
 
 // Manera de mutar con un .map
-
-const letters = ["a", "b", "c"];
-const newArrayMap = letters.map((item) => item + "++");
+const letters4 = ["a", "b", "c"];
+const newArrayMap = letters4.map((item) => item + "++");
 
 console.log("original", letters);
 console.log("arrayVacia", newArrayMap);
+
+// Por medio de un ejercicio
+const solution(array) = [4,8,10,12];
+
+function solution(array) {
+ return newSolution = array.map((item => item*2));
+}; 
+console.logsolution([2, 4, 5, 6]);
