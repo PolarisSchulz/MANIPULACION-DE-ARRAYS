e2776c301d7390c385c4a63c41b5a2f43100d56a
Abigail Peña <abigaildisenografico@gmail.com>
2022-05-29 16:32:16 -0500

    every

--- MANIPULACION DE ARRAYS/server/08-Every.js
+++ MANIPULACION DE ARRAYS/server/08-Every.js
@@ -4,30 +4,30 @@
 // Solamente retorna un true o un false
 
 
-const numbers = [1, 30, 39, 29, 10, 13];
+const numbers4 = [1, 30, 39, 29, 10, 13];
 
-let respuesta = true;
-for (let index = 0; index < numbers.length; index++) {
-    const element = numbers [index];
+let respuesta4 = true;
+for (let index = 0; index < numbers4.length; index++) {
+    const element = numbers4 [index];
     if (element >= 40){
-        respuesta = false
+        respuesta4 = false
     }
 }
-console.log('usando for', respuesta);
+console.log('usando for', respuesta4);
 // Devuelve True
 
 
 
-const numbers = [1, 30, 59, 29, 10, 13];
+const numbers5 = [1, 30, 59, 29, 10, 13];
 
-let respuesta = true;
-for (let index = 0; index < numbers.length; index++) {
-    const element = numbers [index];
+let respuesta5 = true;
+for (let index = 0; index < numbers5.length; index++) {
+    const element = numbers5 [index];
     if (element >= 40){
-        respuesta = false
+        respuesta5 = false
     }
 }
-console.log('usando for', respuesta);
+console.log('usando for', respuesta5);
 // Devuelve False (ya que 59 no es un numero menor a 40)
 
 
@@ -36,18 +36,18 @@ console.log('usando for', respuesta);
 
 
 
-const numbers = [1, 30, 39, 29, 10, 13];
+const numbers6 = [1, 30, 39, 29, 10, 13];
 // () pedimos que nos de directamente la condicion que quiere que todos cumpla
-const respuesta2 = numbers.every(item => item <= 40)
-console.log('usando every', respuesta2)
+const respuesta6 = numbers6.every(item => item <= 40)
+console.log('usando every', respuesta6)
 // Devuelve True
 
 
 
-const numbers = [1, 30, 49, 29, 10, 13];
+const numbers7 = [1, 30, 49, 29, 10, 13];
 // () pedimos que nos de directamente la condicion que quiere que todos cumpla
-const respuesta2 = numbers.every(item => item <= 40)
-console.log('usando every', respuesta2)
+const respuesta7 = numbers7.every(item => item <= 40)
+console.log('usando every', respuesta7)
 // Devuelve False (ya que 49 no es un numero menor a 40)
 
 
@@ -55,22 +55,22 @@ console.log('usando every', respuesta2)
 // ---------------------
 
 const team = [
-  {
+{
     name: "Nicolas",
     age: 12,
-  },
-  {
+},
+{
     name: "Andrea",
     age: 8,
-  },
-  {
+},
+{
     name: "Zulema",
     age: 2,
-  },
-  {
+},
+{
     name: "Santiago",
     age: 18,
-  },
+},
 ];
 
 const teamMenor = team
@@ -79,3 +79,4 @@ const teamMenor = team
 
 teamMenor ? 'El equipo puede participar ya que sus jugadores tiene menor oh igual a 15 años cumple con el requisito' : 'El equipo no puede participar ya que uno o mas de sus jugadores no cumple con el requisito de tener menor a oh igual 15 años'
 // retorna 'El equipo no puede participar ya que uno o mas de sus jugadores no cumple con el requisito de tener menor a oh igual 15 años'
+
