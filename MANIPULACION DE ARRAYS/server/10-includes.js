e708615507333896276807db71bef78efc7ff9f9
Abigail Peña <abigaildisenografico@gmail.com>
2022-05-29 16:49:52 -0500

    includes

---
+++ MANIPULACION DE ARRAYS/server/10-includes.js
@@ -0,0 +1,31 @@
+// Me dice si un elemento está o no está dentro del array (sistema de busqueda)
+
+
+
+const pets = ['cat','dog','bat'];
+
+let includeInArray = false;
+for (let index = 0; index < pets.length; index++) {
+    const element = pets[index];
+    if (element === 'dog'){
+        includeInArray = true;
+        break;
+    }
+}
+console.log('con for', includeInArray);
+// regresa 'for' true
+
+
+
+const pets2 = ['cat','dog','bat'];
+// Con includes no necesitamos un arrow function, solo el elemento
+
+// Al no usarr arrow function no podriamos trabajar con objetos, a menos que pasemos la referencia del objeto directamente
+
+// includes tambien funciona con strings y ver si un elemento incluye una palabra en especifico, por ejemplo
+const respuesta14 = pets2.includes('dog');
+console.log('con include', respuesta14);
+// regresa 'include' true
+
+
+
